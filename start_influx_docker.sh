39451922d62def4856a320941e3983c6139afc25
Naren Mudivarthy <naren-m@users.noreply.github.com>
2017-05-03 08:16:59 -0700

    Update start_influx_docker.sh

--- start_influx_docker.sh
+++ start_influx_docker.sh
@@ -1,6 +1,6 @@
 docker run -d -p 8083:8083 -p 8086:8086 \
           -v $PWD/influxdb:/var/lib/influxdb \
           --name=influxdb \
-          --net=robot \
+          --net=influxdb \
           --rm \
           influxdb
