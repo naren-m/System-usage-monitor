79406d140e95435a8e0744ee0ef98e295b1aa7e1
Naren Mudivarthy <narenuday595@gmail.com>
2017-05-21 15:49:46 -0700

    Cleanup

--- start_grafana_docker.sh
+++ start_grafana_docker.sh
@@ -1,3 +1,5 @@
-# docker run -d --name=grafana --rm -p 3000:3000 --net=robot narenm/grafana
-docker run -d -i --net=influx -p 3000:3000 -v $PWD/grafana:/var/lib/grafana --name grafana grafana/grafana
-
+# docker run -d --name=grafana --rm -p 3009:3009 --net=influxdb narenm/grafana
+docker run -d -i --net=influxdb \
+                 -p 3000:3000 \
+                 -v $PWD/grafana:/var/lib/grafana \
+                 --name grafana grafana/grafana
