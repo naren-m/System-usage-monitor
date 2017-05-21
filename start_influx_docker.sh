17b0542a31f17f4a3cd3abb1a8c69aa4004e03d6
Naren Mudivarthy <narenuday595@gmail.com>
2017-05-21 16:30:23 -0700

    CPU utilization dashboard

--- start_influx_docker.sh
+++ start_influx_docker.sh
@@ -1,6 +1,7 @@
 docker run -d -p 8083:8083 -p 8086:8086 \
+          -v $PWD/influxdb.conf:/etc/influxdb/influxdb.conf:ro \
           -v $PWD/influxdb:/var/lib/influxdb \
           --name=influxdb \
           --net=influxdb \
-          --rm \
-          influxdb
+          -e INFLUXDB_ADMIN_ENABLED=true \
+          influxdb -config /etc/influxdb/influxdb.conf
