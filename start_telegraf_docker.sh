90d6b9d63218245721f9477947b68dcc86d7caee
Naren Mudivarthy <nmudivar@cisco.com>
2017-08-31 19:55:23 -0700

    Added monitoring for docker contianers

--- start_telegraf_docker.sh
+++ start_telegraf_docker.sh
@@ -1,4 +1,5 @@
 docker run -d --name=telegraf \
       --net=influxdb \
       -v $PWD/telegraf.conf:/etc/telegraf/telegraf.conf:ro \
+      -v /var/run/docker.sock:/var/run/docker.sock \
       telegraf
